9b64182213bb84a629f6b5c2919eb8212d6db28e
Gustavo Ballesté Prado <gustavoballeste@MacBook-Pro-de-Gustavo.local>
2018-06-28 00:50:25 -0300

    First commit

---
+++ src/main/java/io/spring/batch/configuration/JobConfiguration.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.spring.batch.configuration;
+
+import javax.sql.DataSource;
+
+import io.spring.batch.domain.Customer;
+import io.spring.batch.domain.CustomerRowMapper;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JobConfiguration {
+
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+
+	@Autowired
+	public DataSource dataSource;
+
+	@Bean
+	public JdbcCursorItemReader<Customer> cursorItemReader() {
+		JdbcCursorItemReader<Customer> reader = new JdbcCursorItemReader<>();
+
+		reader.setSql("select id, firstName, lastName, birthdate from customer order by lastName, firstName");
+		reader.setDataSource(this.dataSource);
+		reader.setRowMapper(new CustomerRowMapper());
+
+		return reader;
+	}
+
+	@Bean
+	public ItemWriter<Customer> customerItemWriter() {
+		return items -> {
+			for (Customer item : items) {
+				System.out.println(item.toString());
+			}
+		};
+	}
+
+	@Bean
+	public Step step1() {
+		return stepBuilderFactory.get("step1")
+				.<Customer, Customer>chunk(10)
+				.reader(cursorItemReader())
+				.writer(customerItemWriter())
+				.build();
+	}
+
+	@Bean
+	public Job job() {
+		return jobBuilderFactory.get("job")
+				.start(step1())
+				.build();
+	}
+}
